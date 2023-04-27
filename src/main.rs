fd874564fb19ae14f293e024be89e30a7c0493d9
lukas <no@email.com>
2023-04-27 21:20:04 +0200

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, error::Error, io::Write};
+use std::error::Error;
 
 #[derive(Clone, Copy)]
 struct Color {
@@ -9,12 +9,10 @@ struct Color {
 struct Image {
     width: usize,
     height: usize,
-    data: Vec<Color>
-
+    data: Vec<Color>,
 }
 
 impl Image {
-
     fn get_mut(&mut self, x: usize, y: usize) -> Option<&mut Color> {
         if x > self.width || y > self.height {
             return None;
@@ -22,7 +20,7 @@ impl Image {
         return self.data.get_mut(y * self.width + x);
     }
 
-    fn get(& self, x: usize, y: usize) -> Option<& Color> {
+    fn get(&self, x: usize, y: usize) -> Option<&Color> {
         if x > self.width || y > self.height {
             return None;
         }
@@ -32,9 +30,18 @@ impl Image {
     fn test_image() -> Self {
         let width = 256;
         let height = 256;
-        let data: Vec<Color> = vec![Color { r: 0.0, g: 0.0, b: 0.0 }; width * height];
+        let data: Vec<Color> = vec![
+            Color {
+                r: 0.0,
+                g: 0.0,
+                b: 0.0
+            };
+            width * height
+        ];
         let mut img = Image {
-            width, height, data
+            width,
+            height,
+            data,
         };
         for y in 0..height {
             eprintln!("lines remaining: {}", height - y);
@@ -42,7 +49,7 @@ impl Image {
                 let r = y as f32 / (width - 1) as f32;
                 let g = x as f32 / (height - 1) as f32;
                 let b = 0.25 as f32;
-                *img.get_mut(x, y).unwrap() = Color {r, g, b};
+                *img.get_mut(x, y).unwrap() = Color { r, g, b };
             }
         }
         img
@@ -62,18 +69,15 @@ impl Image {
     }
 }
 
-
-
-
-fn main() -> Result<(), Box<dyn Error>>{
-
+fn main() -> Result<(), Box<dyn Error>> {
     Image::test_image().write_as_ppm_to_stdout();
     Ok(())
 }
 
 mod geo {
-    use std::{ops};
+    use std::ops;
 
+    #[derive(Clone, Copy, Debug)]
     pub struct Vec3 {
         x: f64,
         y: f64,
@@ -82,11 +86,15 @@ mod geo {
 
     impl Vec3 {
         fn zero() -> Self {
-            return Self {x: 0.0, y: 0.0, z: 0.0};
+            return Self {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            };
         }
 
         fn of(x: f64, y: f64, z: f64) -> Self {
-            return Self {x, y, z};
+            return Self { x, y, z };
         }
 
         fn length_squared(&self) -> f64 {
@@ -95,6 +103,58 @@ mod geo {
 
         fn length(&self) -> f64 {
             self.length_squared().sqrt()
+        }
+
+        fn dot(&self, rhs: &Self) -> f64 {
+            self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+        }
+
+        fn cross(&self, rhs: &Self) -> Self {
+            Self {
+                x: self.y * rhs.z - self.z * rhs.y,
+                y: self.z * rhs.x - self.x * rhs.z,
+                z: self.x * rhs.y - self.y * rhs.x,
+            }
+        }
+        
+        fn unit(&self) -> Self {
+            self / self.length()
+        }
+    }
+
+    impl ops::AddAssign<Vec3> for Vec3 {
+        fn add_assign(&mut self, rhs: Vec3) {
+            self.x += rhs.x;
+            self.y += rhs.y;
+            self.z += rhs.z;
+        }
+    }
+
+    impl ops::Neg for Vec3 {
+        type Output = Vec3;
+
+        fn neg(self) -> Self::Output {
+            Vec3 {
+                x: -self.x,
+                y: -self.y,
+                z: -self.z,
+            }
+        }
+    }
+
+    impl ops::MulAssign<f64> for Vec3 {
+        fn mul_assign(&mut self, rhs: f64) {
+            self.x *= rhs;
+            self.y *= rhs;
+            self.z *= rhs;
+        }
+    }
+
+    impl ops::DivAssign<f64> for Vec3 {
+        fn div_assign(&mut self, rhs: f64) {
+            self.x /= rhs;
+            self.y /= rhs;
+            self.z /= rhs;
         }
     }
 
@@ -110,14 +170,14 @@ mod geo {
         }
     }
 
-    impl ops::Neg for Vec3{
+    impl ops::Sub<Vec3> for Vec3 {
         type Output = Vec3;
 
-        fn neg(self) -> Self::Output {
+        fn sub(self, rhs: Vec3) -> Self::Output {
             Vec3 {
-                x: -self.x, 
-                y: -self.y, 
-                z: -self.z, 
+                x: self.x - rhs.x,
+                y: self.y - rhs.y,
+                z: self.z - rhs.z,
             }
         }
     }
@@ -130,6 +190,30 @@ mod geo {
                 x: self.x * rhs.x,
                 y: self.y * rhs.y,
                 z: self.z * rhs.z,
+            }
+        }
+    }
+
+    impl ops::Mul<Vec3> for f64 {
+        type Output = Vec3;
+
+        fn mul(self, rhs: Vec3) -> Self::Output {
+            Vec3 {
+                x: rhs.x * self,
+                y: rhs.y * self,
+                z: rhs.z * self,
+            }
+        }
+    }
+
+    impl ops::Div<f64> for &Vec3 {
+        type Output = Vec3;
+
+        fn div(self, rhs: f64) -> Self::Output {
+            Vec3 {
+                x: self.x / rhs,
+                y: self.y / rhs,
+                z: self.z / rhs,
             }
         }
     }
