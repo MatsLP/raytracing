1abc68234707f9860e0e0e8c85a89cc56ee6747c
lukas <no@email.com>
2023-05-20 13:40:25 +0200

    place a second ball

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let aspect_ratio = 16.0 / 9.0;
-    let image_width: usize = 400;
+    let image_width: usize = 1980;
     let image_height: usize = (image_width as f64 / aspect_ratio) as usize;
+    
     let mut img = render::Image::empty(image_width, image_height);
 
     let mut scene = scene::Scene::new();
     scene.add_sphere(geo::Vec3::of(0.0, 0.0, -1.0), 0.5);
+    scene.add_sphere(geo::Vec3::of(0.0, -100.5, -1.0), 100.0);
+
 
     render::render(&render::Viewport::default(), &scene, &mut img);
 
